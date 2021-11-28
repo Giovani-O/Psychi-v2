0dd8150d6a256f5988970f96050287588186eb4d
Giovani O <giovani.o@outlook.com.br>
2021-11-28 12:34:14 -0300

    feat(Psychi): Avança na integração

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -15,21 +15,20 @@
 			</h2>
 			</v-col>
 
+
 			<v-col
-			v-for="z in 4"
-			:key="`${x}${z}`"
-			cols="6"
-			md="3"
+				v-for="z in Albuns"
+				:key="`${x}${z}`"
+				cols="6"
+				md="3"
 			>
 				<router-link to="/album">
-					<v-sheet 
+					<v-img
 						width="280"
 						height="280"
-						class="thumbnail mr-16"
-						elevation="3"
-					>
-						<p>Album {{ z }}</p>
-					</v-sheet>
+						:src="z.cover"
+					></v-img>
+					<p> {{ z.artistName }} - {{ z.name }}</p>
 				</router-link>
 			</v-col>
     </template>
@@ -37,60 +36,87 @@
 </template>
 
 <script>
+ import axios from 'axios';
+
 export default {
 	name: 'Dashboard',
 	data() {
 		return {
+			axiosHeaders: {
+				'Content-Type': 'application/json',
+			},
 			Artists: [
 				{
 					code: 1,
-					name: 'Sabaton'
-				},
-				{
-					code: 2,
-					name: 'Rhapsody of Fire'
+					name: 'Albums'
 				},
 			],
 			Albuns: [
-				{
-					artist: 1,
-					code: 1,
-					name: 'Primo Victoria'
-				},{
-					artist: 1,
-					code: 2,
-					name: 'Coat of Arms'
-				},{
-					artist: 1,
-					code: 3,
-					name: 'The Last Stand'
-				},{
-					artist: 1,
-					code: 4,
-					name: 'Heroes'
-				},
-				{
-					artist: 2,
-					code: 5,
-					name: 'Symphony of the Enchanted Lands'
-				},
-				{
-					artist: 2,
-					code: 6,
-					name: 'Dawn of Victory'
-				},
-				{
-					artist: 2,
-					code: 7,
-					name: 'Power of the Dragonflame'
-				},
-				{
-					artist: 2,
-					code: 8,
-					name: 'Rain of a Thousand Flames'
-				}
+				// {
+				// 	artist: 1,
+				// 	code: 1,
+				// 	name: 'Primo Victoria'
+				// },{
+				// 	artist: 1,
+				// 	code: 2,
+				// 	name: 'Coat of Arms'
+				// },{
+				// 	artist: 1,
+				// 	code: 3,
+				// 	name: 'The Last Stand'
+				// },{
+				// 	artist: 1,
+				// 	code: 4,
+				// 	name: 'Heroes'
+				// },
+				// {
+				// 	artist: 2,
+				// 	code: 5,
+				// 	name: 'Symphony of the Enchanted Lands'
+				// },
+				// {
+				// 	artist: 2,
+				// 	code: 6,
+				// 	name: 'Dawn of Victory'
+				// },
+				// {
+				// 	artist: 2,
+				// 	code: 7,
+				// 	name: 'Power of the Dragonflame'
+				// },
+				// {
+				// 	artist: 2,
+				// 	code: 8,
+				// 	name: 'Rain of a Thousand Flames'
+				// }
 			],
 		}
+	},
+	created() {
+		this.getAlbums();
+	},
+	methods: {
+		async getAlbums() {
+        console.log('Loading...')
+        let allAlbums = await axios.get(
+          'http://localhost:3090/Album',
+          { headers: this.axiosHeaders }
+        );
+        console.log(allAlbums.data);
+        console.log('Done...')
+
+        for (let i = 0; i < allAlbums.data.length; i++){
+          this.Albuns.push(
+            {
+              artist: 1,
+							code: i,
+							name: allAlbums.data[i].albumName,
+							artistName: allAlbums.data[i].artistName,
+							cover: allAlbums.data[i].cover,
+            },
+          );
+        }
+      },
 	}
 }
 </script>
